a590d8e2f56d4a9bcf7bb037ec1fd817181a0055
baruchInsert-tech <baruchh@insert-tech.com>
2025-10-22 12:49:00 +0300

    change options to array

--- Assets/YahalomUIPackage/Runtime/BasicDropdown/BasicDropdown.cs
+++ Assets/YahalomUIPackage/Runtime/BasicDropdown/BasicDropdown.cs
@@ -12,21 +12,10 @@ namespace YahalomUIPackage.Runtime.BasicDropdown
         public Action<string> OnValueChanged { get; set; }
 
         [UxmlAttribute]
-        public string options
+        public string[] options
         {
-            get => string.Join(", ", _optionsList);
-
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                {
-                    SetOptions(new List<string>());
-                }
-                else
-                {
-                    SetOptions(value.Split(',').Select(s => s.Trim()).ToList());
-                }
-            }
+            get => _optionsList.ToArray();
+            set => SetOptions(value?.ToList() ?? new List<string>());
         }
 
         [UxmlAttribute]
@@ -48,24 +37,21 @@ namespace YahalomUIPackage.Runtime.BasicDropdown
         public BasicDropdown()
         {
             var styleSheet = Resources.Load<StyleSheet>("BasicDropdown/BasicDropdown");
-            styleSheets.Add(styleSheet);
+            if (styleSheet != null)
+                styleSheets.Add(styleSheet);
 
             AddToClassList("basic-dropdown");
 
-            _button = new VisualElement();
-            _button.name = "Button";
+            _button = new VisualElement { name = "Button" };
             _button.AddToClassList("basic-dropdown-button");
 
-            _label = new Label("Select...");
-            _label.name = "Label";
+            _label = new Label("Select...") { name = "Label" };
             _label.AddToClassList("basic-dropdown-label");
 
-            _icon = new VisualElement();
-            _icon.name = "Icon";
+            _icon = new VisualElement { name = "Icon" };
             _icon.AddToClassList("basic-dropdown-icon");
 
-            _optionsContainer = new VisualElement();
-            _optionsContainer.name = "OptionsContainer";
+            _optionsContainer = new VisualElement { name = "OptionsContainer" };
             _optionsContainer.AddToClassList("basic-dropdown-options");
             _optionsContainer.style.display = DisplayStyle.None;
 
@@ -74,22 +60,22 @@ namespace YahalomUIPackage.Runtime.BasicDropdown
             Add(_button);
             Add(_optionsContainer);
 
-            _button.RegisterCallback<MouseDownEvent>(evt => ToggleDropdown());
+            _button.RegisterCallback<MouseDownEvent>(_ => ToggleDropdown());
 
             focusable = true;
-            RegisterCallback<FocusOutEvent>(evt => CloseDropdown());
+            RegisterCallback<FocusOutEvent>(_ => CloseDropdown());
         }
 
         public void SetOptions(List<string> options)
         {
-            _optionsList = options;
+            _optionsList = options ?? new List<string>();
             _optionsContainer.Clear();
 
             foreach (var option in _optionsList)
             {
                 Label optionLabel = new Label(option);
                 optionLabel.AddToClassList("basic-dropdown-option");
-                optionLabel.RegisterCallback<MouseDownEvent>(evt => { SetValue(option, true); });
+                optionLabel.RegisterCallback<MouseDownEvent>(_ => SetValue(option, true));
 
                 _optionsContainer.Add(optionLabel);
             }
@@ -106,28 +92,23 @@ namespace YahalomUIPackage.Runtime.BasicDropdown
 
         public void SetValue(string value, bool notify = true)
         {
-            if (_value == value) return;
+            if (_value == value)
+                return;
 
             _value = value;
             _label.text = value;
             CloseDropdown();
 
             if (notify)
-            {
                 OnValueChanged?.Invoke(value);
-            }
         }
 
         private void ToggleDropdown()
         {
             if (_isOpen)
-            {
                 CloseDropdown();
-            }
             else
-            {
                 OpenDropdown();
-            }
         }
 
         private void OpenDropdown()
@@ -136,7 +117,7 @@ namespace YahalomUIPackage.Runtime.BasicDropdown
             _isOpen = true;
             _optionsContainer.style.display = DisplayStyle.Flex;
             _icon.EnableInClassList("basic-dropdown-icon--open", true);
-            this.Focus();
+            Focus();
         }
 
         private void CloseDropdown()
