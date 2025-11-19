1950fc5fe1a81189c0c3d5b114eb436a7f0135e7
baruchInsert-tech <baruchh@insert-tech.com>
2025-11-19 15:41:41 +0200

    add gradient

--- Assets/YahalomUIPackage/Runtime/BasicButton/BasicButton.cs
+++ Assets/YahalomUIPackage/Runtime/BasicButton/BasicButton.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using YahalomUIPackage.Runtime.Effects;
 
 namespace YahalomUIPackage.Runtime.BasicButton
 {
@@ -8,7 +9,34 @@ namespace YahalomUIPackage.Runtime.BasicButton
     {
         public const string SelectedClassName = "basic-button--selected";
 
-        public new class UxmlFactory : Button.UxmlFactory { }
+        private GradientBackgroundElement _gradientBackground;
+
+        private Color _topColor = new Color(1f, 1f, 1f, 1f);
+        private Color _bottomColor = new Color(0f, 0f, 0f, 1f);
+
+        [UxmlAttribute("top-color")]
+        public Color TopColor
+        {
+            get => _topColor;
+            set
+            {
+                _topColor = value;
+                if (_gradientBackground != null)
+                    _gradientBackground.TopColor = value;
+            }
+        }
+
+        [UxmlAttribute("bottom-color")]
+        public Color BottomColor
+        {
+            get => _bottomColor;
+            set
+            {
+                _bottomColor = value;
+                if (_gradientBackground != null)
+                    _gradientBackground.BottomColor = value;
+            }
+        }
 
         public bool IsSelected
         {
@@ -19,8 +47,22 @@ namespace YahalomUIPackage.Runtime.BasicButton
         public BasicButton()
         {
             var styleSheet = Resources.Load<StyleSheet>("BasicButton/BasicButton");
-            styleSheets.Add(styleSheet);
+            if (styleSheet != null)
+                styleSheets.Add(styleSheet);
+
             AddToClassList("basic-button");
+
+            _gradientBackground = new GradientBackgroundElement
+            {
+                name = "basic-button-gradient-bg",
+                pickingMode = PickingMode.Ignore
+            };
+
+            _gradientBackground.StretchToParentSize();
+            hierarchy.Insert(0, _gradientBackground);
+
+            _gradientBackground.TopColor = _topColor;
+            _gradientBackground.BottomColor = _bottomColor;
         }
     }
 }
