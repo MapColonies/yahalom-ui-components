7cc0a614a49e17fa8e3536afa7e6ba6de89bcdc1
baruchInsert-tech <baruchh@insert-tech.com>
2025-11-19 19:03:29 +0200

    add colors for each version

--- Assets/YahalomUIPackage/Runtime/BasicButton/BasicButton.cs
+++ Assets/YahalomUIPackage/Runtime/BasicButton/BasicButton.cs
@@ -10,38 +10,106 @@ namespace YahalomUIPackage.Runtime.BasicButton
         public const string SelectedClassName = "basic-button--selected";
 
         private GradientBackgroundElement _gradientBackground;
+        private VisualElement _iconElement;
 
-        private Color _topColor = new Color(1f, 1f, 1f, 1f);
-        private Color _bottomColor = new Color(0f, 0f, 0f, 1f);
+        private Texture2D _iconImage;
+
+        private Color _topColor = Color.white;
+        private Color _bottomColor = Color.black;
+
+        private Color _hoverTopColor = Color.white;
+        private Color _hoverBottomColor = Color.black;
+
+        private Color _selectedTopColor = Color.white;
+        private Color _selectedBottomColor = Color.black;
+
+        private Color _disabledTopColor = new Color(0.3f, 0.3f, 0.3f, 1f);
+        private Color _disabledBottomColor = new Color(0.2f, 0.2f, 0.2f, 1f);
+
+        private bool _isPointerOver;
+
+        [UxmlAttribute("icon-image")]
+        public Texture2D IconImage
+        {
+            get => _iconImage;
+            set
+            {
+                _iconImage = value;
+
+                if (_iconElement == null)
+                    return;
+
+                if (value == null)
+                    _iconElement.style.backgroundImage = StyleKeyword.None;
+                else
+                    _iconElement.style.backgroundImage = new StyleBackground(value);
+            }
+        }
 
         [UxmlAttribute("top-color")]
         public Color TopColor
         {
             get => _topColor;
-            set
-            {
-                _topColor = value;
-                if (_gradientBackground != null)
-                    _gradientBackground.TopColor = value;
-            }
+            set { _topColor = value; UpdateVisualStateInternal(); }
         }
 
         [UxmlAttribute("bottom-color")]
         public Color BottomColor
         {
             get => _bottomColor;
-            set
-            {
-                _bottomColor = value;
-                if (_gradientBackground != null)
-                    _gradientBackground.BottomColor = value;
-            }
+            set { _bottomColor = value; UpdateVisualStateInternal(); }
+        }
+
+        [UxmlAttribute("hover-top-color")]
+        public Color HoverTopColor
+        {
+            get => _hoverTopColor;
+            set { _hoverTopColor = value; UpdateVisualStateInternal(); }
+        }
+
+        [UxmlAttribute("hover-bottom-color")]
+        public Color HoverBottomColor
+        {
+            get => _hoverBottomColor;
+            set { _hoverBottomColor = value; UpdateVisualStateInternal(); }
+        }
+
+        [UxmlAttribute("selected-top-color")]
+        public Color SelectedTopColor
+        {
+            get => _selectedTopColor;
+            set { _selectedTopColor = value; UpdateVisualStateInternal(); }
+        }
+
+        [UxmlAttribute("selected-bottom-color")]
+        public Color SelectedBottomColor
+        {
+            get => _selectedBottomColor;
+            set { _selectedBottomColor = value; UpdateVisualStateInternal(); }
+        }
+
+        [UxmlAttribute("disabled-top-color")]
+        public Color DisabledTopColor
+        {
+            get => _disabledTopColor;
+            set { _disabledTopColor = value; UpdateVisualStateInternal(); }
+        }
+
+        [UxmlAttribute("disabled-bottom-color")]
+        public Color DisabledBottomColor
+        {
+            get => _disabledBottomColor;
+            set { _disabledBottomColor = value; UpdateVisualStateInternal(); }
         }
 
         public bool IsSelected
         {
             get => ClassListContains(SelectedClassName);
-            set => EnableInClassList(SelectedClassName, value);
+            set
+            {
+                EnableInClassList(SelectedClassName, value);
+                UpdateVisualStateInternal();
+            }
         }
 
         public BasicButton()
@@ -57,12 +125,69 @@ namespace YahalomUIPackage.Runtime.BasicButton
                 name = "basic-button-gradient-bg",
                 pickingMode = PickingMode.Ignore
             };
-
             _gradientBackground.StretchToParentSize();
             hierarchy.Insert(0, _gradientBackground);
 
-            _gradientBackground.TopColor = _topColor;
-            _gradientBackground.BottomColor = _bottomColor;
+            _iconElement = new VisualElement
+            {
+                name = "basic-button-icon",
+                pickingMode = PickingMode.Ignore
+            };
+            _iconElement.AddToClassList("basic-button__icon");
+            hierarchy.Add(_iconElement);
+
+            RegisterCallback<PointerEnterEvent>(_ =>
+            {
+                _isPointerOver = true;
+                UpdateVisualStateInternal();
+            });
+            RegisterCallback<PointerLeaveEvent>(_ =>
+            {
+                _isPointerOver = false;
+                UpdateVisualStateInternal();
+            });
+
+            RegisterCallback<AttachToPanelEvent>(_ => UpdateVisualStateInternal());
+
+            UpdateVisualStateInternal();
+        }
+        
+        public void RefreshVisualState()
+        {
+            UpdateVisualStateInternal();
+        }
+
+        private void UpdateVisualStateInternal()
+        {
+            if (_gradientBackground == null)
+                return;
+
+            Color top;
+            Color bottom;
+
+            if (!enabledInHierarchy || !enabledSelf)
+            {
+                top = _disabledTopColor;
+                bottom = _disabledBottomColor;
+            }
+            else if (IsSelected)
+            {
+                top = _selectedTopColor;
+                bottom = _selectedBottomColor;
+            }
+            else if (_isPointerOver)
+            {
+                top = _hoverTopColor;
+                bottom = _hoverBottomColor;
+            }
+            else
+            {
+                top = _topColor;
+                bottom = _bottomColor;
+            }
+
+            _gradientBackground.TopColor = top;
+            _gradientBackground.BottomColor = bottom;
         }
     }
 }
