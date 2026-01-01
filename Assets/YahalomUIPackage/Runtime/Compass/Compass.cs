00f7559ec1b9202adce23ce81bf00b6bc26c9946
baruchInsert-tech <baruchh@insert-tech.com>
2026-01-01 18:12:08 +0200

    rename

--- Assets/YahalomUIPackage/Runtime/Compass/Compass.cs
+++ Assets/YahalomUIPackage/Runtime/Compass/Compass.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace YahalomUIPackage.Runtime.Compass
 {
-    public class CompassController : MonoBehaviour
+    public class Compass : MonoBehaviour
     {
         [SerializeField] private RectTransform _compassTransform;
         [SerializeField] private TMP_Text _angleText;
@@ -12,30 +12,10 @@ namespace YahalomUIPackage.Runtime.Compass
         [SerializeField] private float _startAngle;
         [SerializeField] private float _smoothTime = 0.1f;
 
-        private Vector2 _lastDebugCoordinates; 
+        private Vector2 _lastDebugCoordinates;
         private float _currentVelocity;
         private float _smoothedAngle;
         private int _lastDisplayAngle = -1;
-
-        //TODO: will be removed after product review
-        [Header("Debug")] 
-        [SerializeField] private bool _enableDebugSlider;
-        [Range(-360, 360)] 
-        [SerializeField] private float _debugAngle;
-        [SerializeField] private Vector2 _debugCoordinates;
-
-        private void Start()
-        {
-            if (_compassTransform == null)
-            {
-                Debug.LogError("RectTransform is not assigned!");
-                enabled = false;
-                return;
-            }
-
-            SetHeading(0f);
-            SetCoordinates(Vector2.zero);
-        }
 
         private void UpdateRotation(float targetHeading)
         {
@@ -69,7 +49,7 @@ namespace YahalomUIPackage.Runtime.Compass
             _lastDisplayAngle = currentDisplayAngle;
             _angleText.text = $"{currentDisplayAngle}°NE";
         }
-        
+
         public void SetHeading(float heading)
         {
             UpdateRotation(heading);
@@ -79,21 +59,6 @@ namespace YahalomUIPackage.Runtime.Compass
         public void SetCoordinates(Vector2 latLong)
         {
             SetCoordinatesText($"{latLong.y:F4}°N\\n{latLong.x:F4}°E");
-        }
-        
-        
-        //TODO: will be removed after product review
-        private void Update()
-        {
-            if (_enableDebugSlider)
-            {
-                SetHeading(_debugAngle);
-                if ((_debugCoordinates - _lastDebugCoordinates).sqrMagnitude > 0.0001f)
-                {
-                    _lastDebugCoordinates = _debugCoordinates;
-                    SetCoordinates(_debugCoordinates);
-                }
-            }
         }
     }
 }
